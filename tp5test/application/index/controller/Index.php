d128dbfd1bc9ad08b3adab86e4b01f8cbf9e267a
aqie123 <2924811900@qq.com>
2017-09-30 09:59:18 +0800

    接收彩票接口

--- tp5test/application/index/controller/Index.php
+++ tp5test/application/index/controller/Index.php
@@ -5,6 +5,7 @@ header("content-type:text/html;charset=gbk");
 
 class Index extends Controller
 {
+    const Lottery_API = 'http://f.apiplus.net/ssq.json';
     public function index()
     {
         return $this->fetch();
@@ -27,5 +28,38 @@ class Index extends Controller
         // var_dump($response);die;
         return $response;
 
+    }
+
+    // 获取彩票接口数据
+    public function getLottery(){
+        
+        $httRequest = new \libs\HttpRequest();
+        $response = $httRequest::request(self::Lottery_API);
+        $opencode = [];
+        $response = json_decode($response,true);
+        foreach($response['data'] as $k =>&$v){
+            $v['opencode']=preg_replace("/[+]/",',',$v['opencode']);
+            //$opencode[] = $v['opencode'];
+        }
+        //return json_encode($opencode);
+        return json_encode($response['data']);
+    }
+
+    public function getLottery2(){
+        // http://www.opencai.net/apifree/
+        $lottery_url = 'http://f.apiplus.net/ssq.json';
+        echo '<br>采集地址：'.$lottery_url.'<br>';
+        $lottery_url .= '?_='.time();
+        $json = file_get_contents(urldecode($lottery_url));
+        $json = json_decode($json);
+        echo "<br>".date('Y-m-d H:i:s')."共采集到{$json->rows}行开奖数据：<br>";
+
+        for ($i = 0; $i < count($json->data); $i++) {
+            $p = $json ->data[$i]->expect;
+            echo '<br>开奖期号：'.substr($p,0,8).'-'.substr($p,-3,3);
+            echo '<br>开奖号码：'.$json ->data[$i]->opencode;
+            echo '<br>开奖时间：'.$json ->data[$i]->opentime;
+            echo '<br>';
+        }
     }
 }
